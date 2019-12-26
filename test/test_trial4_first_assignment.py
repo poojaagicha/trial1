a3ca255bd52324e4af16aaeea497ed19c7a61557
Pooja <pooja.m.agicha@gmail.com>
2019-12-26 21:25:31 +0000

    commit 6

--- test/test_trial4_first_assignment.py
+++ test/test_trial4_first_assignment.py
@@ -1,5 +1,5 @@
 import pytest
-from trial4_first_assignment import dividenum
+from trial4_first_assignment import dividenum, multiplynum
 
 @pytest.mark.parametrize("x,y,result",
 [(40,10,4),
@@ -7,5 +7,15 @@ from trial4_first_assignment import dividenum
 pytest.param(90,10, 9, marks=pytest.mark.xfail)
 ]
                          )
-def test_all(x,y,result):
+def test_divide(x,y,result):
     assert dividenum(x,y) == result
+
+@pytest.mark.parametrize("x,y,expected",
+[(20,10,200),
+ (5,8,40),
+pytest.param(90,10, 110, marks=pytest.mark.xfail)
+ ]
+                        )
+def test_multiply(x,y,expected):
+    assert multiplynum(x,y) == expected
+
