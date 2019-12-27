4a00731eb76a2e513820d3cf4be2e41474a195d1
Pooja <pooja.m.agicha@gmail.com>
2019-12-27 13:13:08 +0000

    add and substract functionality added and formatted

--- src/trial4_first_assignment.py
+++ src/trial4_first_assignment.py
@@ -7,3 +7,13 @@ def dividenum(x, y):
 def multiplynum(x, y):
     multiply = x * y
     return multiply
+
+
+def addnumber(x, y):
+    add = x + y
+    return add
+
+
+def subnum(x, y):
+    sub = x - y
+    return sub
